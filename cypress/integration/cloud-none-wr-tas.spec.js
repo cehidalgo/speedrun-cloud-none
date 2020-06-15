6d1e65f12c1057e8e68295eab5cd155883255dc0
Carlos Hidalgo <carlos@formahomolog.com.br>
2020-06-15 19:31:26 -0300

    init repository ~ world record on execution of 'npm run film'

---
+++ cypress/integration/cloud-none-wr-tas.spec.js
@@ -0,0 +1,10 @@
+const BASE_URL = "https://www.monstercat.com/";
+https: describe("Get the world record in accessing Speed Run by CloudNone and playing it", () => {
+   it("Should get the world record!!!!!11", () => {
+      cy.visit(BASE_URL);
+
+      cy.get("body > header > div.container.player > div.col-xs-hidden.col-md-visible.global-search > form > input[type=text]").type("Speed Run");
+      cy.get("body > header > div.container.player > div.col-xs-hidden.col-md-visible.global-search > form > button > i").click();
+      cy.get("body > section > div > div.panel.panel--mobile > div > table > tbody > tr > td.shrink > button > i").click();
+   });
+});
